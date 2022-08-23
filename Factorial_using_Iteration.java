ac0b5b0b2d27ca0ef4b8105248da3f4f728badf9
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-24 00:06:47 +0530

    Update Factorial_using_Iteration.java

--- Factorial_using_Iteration.java
+++ Factorial_using_Iteration.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+public class Factorial {
+    public static void main(String[] args) {
+        int fact = 1, i;
+        System.out.println("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(i = 1; i <= num; i++){
+            fact = fact*i;
+        }
+        System.out.println("The Factorial of " + num + " is " + fact);
+    }
+}
