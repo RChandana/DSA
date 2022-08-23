e83d11a6fae3ead968f04b4e0738ab918a01cdb1
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-24 00:02:54 +0530

    Update Factorial_using_Recursion.java

--- Factorial_using_Recursion.java
+++ Factorial_using_Recursion.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int factorial = multiplication(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static int multiplication(int num){
+        if (num >= 1){
+            return num * multiplication(num - 1);
+        }
+        else
+            return 1;
+    }
+}
