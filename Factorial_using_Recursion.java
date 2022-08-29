c32d23c66c148dbd7ec82ac2b4fd1ae2d6149043
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-30 01:15:26 +0530

    Update Factorial_using_Recursion.java

--- Factorial_using_Recursion.java
+++ Factorial_using_Recursion.java
@@ -15,3 +15,20 @@ public class Factorial {
             return 1;
     }
 }
+
+
+// T(n) = T(n-1) + 3    (3 is for three constant operations like multiplication, subtraction and checking the value of n in each recursive call.) 
+
+// = T(n-2) + 6 (Second recursive call) 
+// = T(n-3) + 9 (Third recursive call) 
+// .
+// . 
+// . 
+// . 
+// = T(n-k) + 3k 
+// till, k = n 
+// Then, = T(n-n) + 3n 
+//           = T(0) + 3n 
+//           = 1 + 3n
+// Therefore, 
+// The time complexity is O(n).
