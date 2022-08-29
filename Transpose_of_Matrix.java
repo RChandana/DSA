988796edb011b0138991c3e4c9eb505ce12aaf51
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-30 01:13:15 +0530

    Update Transpose_of_Matrix.java

--- Transpose_of_Matrix.java
+++ Transpose_of_Matrix.java
@@ -5,4 +5,13 @@ public class Lab_1_Transpose_Matrix {
         for (i = 0; i < n; i++)
             for (j = 0; j < n; j++)
                 N[i][j] = M[j][i];
-    }   
+    }
+    
+    public static void main (String[] args){
+        int M[][] = {{1, 2, 3},
+                     {4, 5, 6},
+                     {7, 8, 9}};
+     
+        int N[][] = new int[n][n], i, j;
+     
+        transpose(M, N);
