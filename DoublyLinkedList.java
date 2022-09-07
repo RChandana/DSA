9eeaecdd532ecd83bf737dd00f652b91fecb9b90
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:08:35 +0530

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -19,3 +19,26 @@ public class Lab_2_Doubly_LinkedList {
         head.prev = newNode;
       head = newNode;
     }
+    
+    public void insertAfter(Node prev_node, int data) {
+      if (prev_node == null) {
+        System.out.println("Previous node cannot be null");
+        return;
+      }
+  
+      Node new_node = new Node(data);
+      new_node.next = prev_node.next;
+      prev_node.next = new_node;
+      new_node.prev = prev_node;
+      if (new_node.next != null)
+        new_node.next.prev = new_node;
+    }
+    void insertEnd(int data) {
+      Node new_node = new Node(data);
+      Node temp = head;
+      new_node.next = null;
+      if (head == null) {
+        new_node.prev = null;
+        head = new_node;
+        return;
+      }
