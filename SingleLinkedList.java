03a60ed2593017f00ce039f077eb9a73f14503fe
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:12:50 +0530

    Update SingleLinkedList.java

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -9,4 +9,21 @@ class LinkedList{
       next = null;
     }
   }
+  
+  public void insertAtBeginning(int new_data){
+    
+    Node new_node = new Node(new_data);
+    new_node.next = head;
+    head = new_node;
+  }
+
+  public void insertAfter(Node prev_node, int new_data){
+    if (prev_node == null){
+      System.out.println("The given previous node cannot be null");
+      return;
+    }
+    Node new_node = new Node(new_data);
+    new_node.next = prev_node.next;
+    prev_node.next = new_node;
+  }
 
