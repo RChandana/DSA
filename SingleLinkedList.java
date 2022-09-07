61ca7ef8c126fd010133ff4127893b74c1b1c9c5
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:13:38 +0530

    Update SingleLinkedList.java

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -59,6 +59,17 @@ class LinkedList{
     Node next = temp.next.next;
     temp.next = next;
   }
+  
+  boolean search(Node head, int key){
+    Node current = head;
+    while (current != null){
+      if (current.data == key)
+        return true;
+      current = current.next;
+      System.out.println("Searched a node ");
+    }
+    return false;
+  }
 
   
   
