5b509ce7cf996fd94238c685c511e1ed8c7b3f0d
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:16:02 +0530

    Update StackUsingArray.java

--- StackUsingArray.java
+++ StackUsingArray.java
@@ -1,1 +1,11 @@
+class Stack_2{
+    private int arr[];
+    private int top;
+    private int capacity;
+
+    Stack_2(int size) {
+      arr = new int[size];
+      capacity = size;
+      top = -1;
+    }
 
