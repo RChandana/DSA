899f0c66610d46f490fd68127598b38b6c1dfdbd
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:10:58 +0530

    Update QueueUsingLinkedList.java

--- QueueUsingLinkedList.java
+++ QueueUsingLinkedList.java
@@ -24,3 +24,17 @@ public class Lab_2_Queue {
         System.out.println("Element " + data+ " removed from the queue");
         return data;
     }
+    public void enqueue(int data){
+        Node oldRear = rear;
+        rear = new Node();
+        rear.data = data;
+        rear.next = null;
+        if (isEmpty()){
+            front = rear;
+        }
+        else {
+            oldRear.next = rear;
+        }
+        queueSize++;
+        System.out.println("Element " + data+ " added to the queue");
+    }
