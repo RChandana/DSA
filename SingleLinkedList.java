5b30d12790189d7fcd6fb93c80f5df9357c65516
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:13:24 +0530

    Update SingleLinkedList.java

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -44,4 +44,21 @@ class LinkedList{
     return;
   }
   
+  void deleteNode(int position){
+    if (head == null)
+      return;
+    Node temp = head;
+    if (position == 0){
+      head = temp.next;
+      return;
+    }
+    for (int i = 0; temp != null && i < position - 1; i++)
+      temp = temp.next;
+    if (temp == null || temp.next == null)
+      return;
+    Node next = temp.next.next;
+    temp.next = next;
+  }
+
+  
   
