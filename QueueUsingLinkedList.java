8855ceb2826cd0acdd2ab236d0ab63404c7609e2
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 11:10:30 +0530

    Update QueueUsingLinkedList.java

--- QueueUsingLinkedList.java
+++ QueueUsingLinkedList.java
@@ -1,1 +1,16 @@
-
+public class Lab_2_Queue { 
+    private Node front, rear; 
+    private int queueSize; 
+    
+    private class Node { 
+        int data;
+        Node next;
+    }
+    public Lab_2_Queue(){
+        front = null;
+        rear = null;
+        queueSize = 0;
+    }
+    public boolean isEmpty(){
+        return (queueSize == 0);
+    }
