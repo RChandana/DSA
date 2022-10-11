202275178cf83d386d676e20d646dd48c7bf3dac
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-11 18:29:43 +0530

    Update Queues_Operations.java

--- Queues_Operations.java
+++ Queues_Operations.java
@@ -1,1 +1,26 @@
-
+public class Lab_2_Queue { 
+    private Node front, rear; 
+    private int queueSize; 
+    
+    private class Node { 
+        int data;
+        Node next;
+    }
+    public Lab_2_Queue(){
+        front = null;
+        rear = null;
+        queueSize = 0;
+    }
+    public boolean isEmpty(){
+        return (queueSize == 0);
+    }
+    public int dequeue(){
+        int data = front.data;
+        front = front.next;
+        if (isEmpty()){
+            rear = null;
+        }
+        queueSize--;
+        System.out.println("Element " + data+ " removed from the queue");
+        return data;
+    }
