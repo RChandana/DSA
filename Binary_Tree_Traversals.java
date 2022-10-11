90b340044523a35630f861b8e34b6f6107333475
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-11 18:21:30 +0530

    Update Binary_Tree_Traversals.java

--- Binary_Tree_Traversals.java
+++ Binary_Tree_Traversals.java
@@ -1,1 +1,9 @@
-
+public class BinaryTreeTraversal {
+    int item;
+    BinaryTreeTraversal left, right;
+  
+    public BinaryTreeTraversal(int key) {
+    item = key;
+    left = right = null;
+    }
+  }
