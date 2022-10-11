248b39dfdc41c3472e3ce1be8b224281f46a8051
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-11 18:25:37 +0530

    Update Circular_Queues_Operations.java

--- Circular_Queues_Operations.java
+++ Circular_Queues_Operations.java
@@ -1,1 +1,19 @@
+public class CircularQueue {
+    int SIZE = 5;
+    int front, rear;
+    int items[] = new int[SIZE];
 
+    CircularQueue(){
+        front = -1;
+        rear = -1;
+    }
+
+    boolean isFull() {
+        if(front == 0 && rear == SIZE - 1){
+            return true;
+        }
+        if(front == rear + 1){
+            return true;
+        }
+        return false;
+    }
