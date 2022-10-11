92f1285bf5e7aa5a4af0a59786bfe734a9ac74cc
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-11 18:23:07 +0530

    Update Binary_Tree_Traversals.java

--- Binary_Tree_Traversals.java
+++ Binary_Tree_Traversals.java
@@ -46,3 +46,25 @@ public class BinaryTreeTraversal {
    
     pre_order(node.right);
     }
+      
+          public static void main(String[] args) {
+    BinaryTree tree = new BinaryTree();
+    tree.root = new BinaryTreeTraversal(4);
+    tree.root.left = new BinaryTreeTraversal(23);
+    tree.root.right = new BinaryTreeTraversal(13);
+    tree.root.left.left = new BinaryTreeTraversal(7);
+    tree.root.left.right = new BinaryTreeTraversal(11);
+  
+    System.out.println("The Inorder traversal of the Binary Tree is  : ");
+    tree.inorder(tree.root);
+    System.out.println("  ");
+  
+    System.out.println("\nThe Preorder traversal of the Binary Tree is  : ");
+    tree.pre_order(tree.root);
+    System.out.println("  ");
+  
+    System.out.println("\nThe Postorder traversalof the Binary Tree is  : ");
+    tree.post_order(tree.root);
+    System.out.println("  ");
+    }
+  }
