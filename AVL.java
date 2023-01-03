f619f1b64b8b79d88c92cf9721702e55f52a1dfe
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-03 13:03:01 +0530

    Update AVL.java

--- AVL.java
+++ AVL.java
@@ -7,3 +7,26 @@ Class Node{
         height = 1;
     }
 }
+
+class AVL{
+ 
+    Node root;
+    int height(Node N) {
+        if (N == null)
+            return 0;
+        return N.height;
+    }
+    
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    Node right_Rotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+        x.right = y;
+        y.left = T2;
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+        return x;
+    }
