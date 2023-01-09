8d074a0f5bd1f131c190f790b588595771391eeb
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-09 23:18:26 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -12,3 +12,14 @@ public class DFS{
     void addEdge(int v, int w){
         adj[v].add(w); 
     }
+    
+    void DFS_Util(int v, boolean visited[]){
+        visited[v] = true;
+        System.out.print(v + " ");
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+               DFS_Util(n, visited);
+           }
+    }
