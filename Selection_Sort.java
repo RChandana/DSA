70261797b966509d2b878f7bdbda94195809e3f9
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-09 23:44:02 +0530

    Update Selection_Sort.java

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -1,1 +1,16 @@
 // Write a program that takes the details of Students (name, roll number, address, CGPA) and sort it in a non-decreasing order using Selection sort based on CGPA.  
+
+import java.util.*;
+class student_record {
+    String name;
+    int roll_number;
+    String address;
+    double CGPA;
+
+    student_record(String name, int roll_number, String address, double CGPA){
+        super();
+        this.name = name;
+        this.roll_number = roll_number;
+        this.address = address;
+        this.CGPA = CGPA;
+    }
