f3ab131095a4d1654ee53e91b0293821b172101f
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-09 23:18:15 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -1,1 +1,14 @@
-
+import java.io.*;
+import java.util.*;
+public class DFS{
+    private int Vertices; 
+    private LinkedList<Integer> adj[];
+    @SuppressWarnings("unchecked") DFS(int v){
+     Vertices = v;
+     adj = new LinkedList[v];
+     for (int i = 0; i < v; ++i)
+        adj[i] = new LinkedList();
+    }
+    void addEdge(int v, int w){
+        adj[v].add(w); 
+    }
