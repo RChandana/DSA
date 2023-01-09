98d5c7453a9ee98393410aa5d8e471a1bdbd6aeb
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-09 23:55:40 +0530

    Update Quick_Sort.java

--- Quick_Sort.java
+++ Quick_Sort.java
@@ -1,1 +1,14 @@
+import java.util.Arrays;
+class Quick_Sort {
+  static int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+      if (array[j] <= pivot) {
+        i++;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
 
+    }
